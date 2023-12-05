6dd5486e5eee732b5af5b17f078a0c4a3b948bfb
Guo Tengda <ttguotengda@foxmail.com>
2023-12-05 12:58:04 +0800

    Update utils.go

--- utils/utils.go
+++ utils/utils.go
@@ -1,1 +1,22 @@
+package utils
 
+import (
+    "reflect"
+)
+
+// ToAnySlice return a slice type of []any
+// no matter of the type of input ([]uint64/string)
+func ToAnySlice(slice any) []any {
+    s := reflect.ValueOf(slice)
+    if s.Kind() := reflect.Slice {
+      panic("Given a non-slice type")
+    }
+    if s.IsNil(){
+      return nil
+    }
+    r := make([]any, s.Len())
+    for i:=0; i<s.Len(); i++ {
+      r[i] = s.Index(i).Interface()
+    }
+    return r
+}
